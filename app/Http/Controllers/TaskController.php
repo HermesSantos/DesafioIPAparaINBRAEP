8dfe4b9eefab488c7248a1d710dd3520d4f2f815
Hermes Santos <hermessantos995@gmail.com>
2022-04-07 15:59:02 -0300

    primeiro commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+//Task model chamada como ModelsTask
+use App\Models\Task as ModelsTask;
+use Illuminate\Http\Request;
+use App\Task;
+use Illuminate\Queue\Events\JobRetryRequested;
+
+class TaskController extends Controller
+{
+
+    //metodo para listagem de tasks apenas com status ok após tarefa ser feita
+    public function getOkTask(){
+        return response()->json([ModelsTask::where('status','ok')->get()]);
+    }
+
+    //metodo para listagem de task
+    public function getTask(){
+        //uma outra linha que possa funcionar em caso de erro na database: return ModelsTask::all();
+        return response()->json([ModelsTask::all(), 200]);
+    }
+
+    //metodo que retorna a task por id
+    public function getTaskById($id){
+        $task = ModelsTask::find($id);
+        //verificação de id inválido
+        if(is_null($task)){
+            return response()-> json(['message' => 'Task não encontrada!!!', 404]);
+        }
+        return response()->json([$task::find($id), 200]);
+    }
+
+    //metodo para adição de task
+    public function addTask(Request $request){
+        $task = ModelsTask::create($request->all());
+        return response($task, 201);
+    }
+
+    //metodo para o update de uma task por sua id
+    public function updateTask(Request $request, $id){
+        $task = ModelsTask::find($id);
+        if(is_null($task)){
+            return response()-> json(['message' => 'Task não encontrada.', 404]);
+        }
+        $task -> update($request->all());
+        return response($task, 201);
+    }
+
+    public function deleteTask(Request $request, $id){
+        $task = ModelsTask::find($id);
+        if(is_null($task)){
+            return response()-> json(['message' => 'Task não encontrada.', 404]);
+        }
+        $task->delete();
+        return response()->json(['message'=>'task deletada com sucesso', 200]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+//Rota GET teste para listagem de tasks com 'ok' no status
+Route::get('oktasks',[TaskController::class, 'getOkTask']);
+
+//Rota GET para as tasks
+Route::get('tasks', [TaskController::class, 'getTask']);//ou com ->name('tasks');
+
+//Rota GET para task específica
+Route::get('tasks/{id}', [TaskController::class, 'getTaskById']);
+
+//Rota POST para cadastrar nova task
+Route::post('addtask', [TaskController::class, 'addTask']);
+
+//Rota PUT para atualização de tarefa task
+Route::put('updatetask/{id}', [TaskController::class, 'updateTask']);
+
+//Rota DELETE para exclusão de task
+Route::delete('delete/{id}',[TaskController::class, 'deleteTask']);
